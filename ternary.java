f39c195cd4540ecf12f39ad1daa84c4c2a9cde1a
sakshi-badave <202401110019@mitaoe.ac.in>
2025-06-18 09:57:32 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,8 @@
+public class ternary {
+    public static void main(String[] args) {
+        int a=40,b=60;
+        String ans;
+        ans=a>b?"a is greater":"b is greater";
+        System.out.println(ans);
+    }
+}
