cb7cc73307932fb16776cc960075900566405c41
sakshi-badave <202401110019@mitaoe.ac.in>
2025-06-18 09:56:12 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,10 @@
+public class whileloop {
+   public static void main(String[] args) {
+    System.out.println("First 10 natural numbers are:");
+    int i=1;
+    while (i<=10) {
+        System.out.println(i);
+        i++;
+    }
+   }
+}
