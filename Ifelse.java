967313fc7ea3ce3f9ea1893d88799ff68f35a6bd
sakshi-badave <202401110019@mitaoe.ac.in>
2025-06-18 09:54:55 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,11 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        int a=10;
+        if(a%2==0){
+            System.out.println(a+" It is a even number");
+        }
+        else{
+            System.out.println(a+" It is an odd number");
+        }
+    }
+}
