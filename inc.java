42eb1481bccaf71499b7eb937f17f6224109e76f
sakshi-badave <202401110019@mitaoe.ac.in>
2025-06-18 09:51:23 +0530

    Add files via upload

---
+++ inc.java
@@ -0,0 +1,11 @@
+public class inc {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println("Original value of a "+a);
+        a++;
+        System.out.println("Increamented value of a is "+a);
+        a=10;
+        a--;
+        System.out.println("Decreamented value of a is "+a);
+    }
+}
