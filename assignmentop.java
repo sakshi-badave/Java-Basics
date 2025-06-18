fb5a54467730fcef8a72a6467a64e3e09264e5cf
sakshi-badave <202401110019@mitaoe.ac.in>
2025-06-18 09:53:40 +0530

    Add files via upload

---
+++ assignmentop.java
@@ -0,0 +1,8 @@
+public class assignmentop {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        a+=b;
+        System.out.println("New value of a is "+a);
+    }
+}
