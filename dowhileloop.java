27dfde360b37acba4ba45df46e574f1180f59014
sakshi-badave <202401110019@mitaoe.ac.in>
2025-06-18 09:56:29 +0530

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,11 @@
+public class dowhileloop {
+    public static void main(String[] args) {
+        System.out.println("First 10 natural numbers are:");
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<=10);
+    }
+}
