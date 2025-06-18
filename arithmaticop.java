3c13f8f0d209bdf9245980d8608cc66bbe20f4cc
sakshi-badave <202401110019@mitaoe.ac.in>
2025-06-18 09:54:05 +0530

    Add files via upload

---
+++ arithmaticop.java
@@ -0,0 +1,9 @@
+public class arithmaticop {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        System.out.println("Summtion "+(a+b));
+        System.out.println("Multiplication "+(a*b));
+        System.out.println("Substraction "+(b-a));
+        System.out.println("Division "+(b/a));
+    }
+}
