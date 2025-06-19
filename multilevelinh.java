4124373ba1a48cb0f8bae49a810f5ded0b3513d0
sakshi-badave <202401110019@mitaoe.ac.in>
2025-06-19 14:47:54 +0530

    Add files via upload

---
+++ multilevelinh.java
@@ -0,0 +1,30 @@
+class parent{
+    void hi(){
+        System.out.println("Hi I am Sakshi");
+    }
+}
+class child extends parent{
+    void intro(){
+        System.out.println("I am student at MITAOE");
+    }  
+  
+}
+class grandchild extends child{
+    void coure(){
+        System.out.println("I am studing AIML");
+    }
+}
+public class multilevelinh {
+       public static void main(String[] args) {
+    // parent p= new parent();
+    // p.hi();
+    // child c=new child();
+    // c.hi();
+    // c.intro();
+    grandchild g=new grandchild();
+    g.hi();
+    g.intro();
+    g.coure();
+   }
+    
+}
